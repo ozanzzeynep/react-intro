f8a0f1b521b7c82f9144e6b6326fcf79815c4c0b
ZeynepOzan <ozanzzeynep@gmail.com>
2023-12-27 16:34:56 +0300

    then catch finally

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,9 +5,9 @@ export default function Navbar() {
       <nav className="navbar bg-dark border-bottom border-body navbar-expand-lg bg-body-tertiary"
       data-bs-theme="dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <a className="navbar-brand" to="/">
             Navbar
-          </a>
+            </a>
           <button
             className="navbar-toggler"
             type="button"
@@ -23,48 +23,12 @@ export default function Navbar() {
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#">
-                  Home
+                  Ana Sayfa
                 </a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#">
-                  Link
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" aria-disabled="true">
-                  Disabled
+                  Hakkında
                 </a>
               </li>
             </ul>
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function Homepage() {
+
+    const myAsyncFunction = () => {
+        return new Promise((resolve,reject) => {
+            resolve("HTTP Verisi");
+
+        });
+    };
+    useEffect(() => {
+        myAsyncFunction()
+        .then(response => {
+            console.log("İşlem başarılı cevap : " ,response);
+        })
+        .catch(error => {
+            console.log("İşlem başarısız cevap : ",error);
+        })
+        .finally(()=> {
+            console.log("İşlem bitti.")
+        });
+    },[]);
+
   return (
     <div>
       <button className="btn btn-primary">Homepage</button>
