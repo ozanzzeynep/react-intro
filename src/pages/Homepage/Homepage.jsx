26d2ac0843e30222ffe7508b8a9df56de654a3fe
ZeynepOzan <ozanzzeynep@gmail.com>
2023-12-28 18:03:09 +0300

    get all product

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,61 +1,27 @@
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import ProductCard from "../../components/ProductCard/ProductCard";
+import axios from "axios";
 
 export default function Homepage() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetchProducts();
+  });
 
-    const myAsyncFunction = () => {
-        return new Promise((resolve,reject) => {
-            resolve("HTTP Verisi");
-
-        });
-    };
-    useEffect(() => {
-       makeAsyncCall();
-       makeHttpCall();
-       makeHttpCall2();
-    },[]);
-
-    const makeHttpCall = () => {
-        fetch("https://dummyjson.com/products")
-        .then(response => response.json())
-        .then(json => console.log(json))
-        .catch(err => console.log(err));
-
-    };
-
-
-    const makeHttpCall2 = async () => {
-        let response = await fetch ("https://dummyjson.com/products");
-        let json = await response.json();
-        console.log(json);
-    }
-
-    const makeAsyncCall = async () => {
-        //myAsyncFunction()
-        //.then(response => {
-        //    console.log("İşlem başarılı cevap : " ,response);
-        //})
-        //.catch(error => {
-        //    console.log("İşlem başarısız cevap : ",error);
-        //})
-        //.finally(()=> {
-        //    console.log("İşlem bitti.")
-        //});
-        try{
-            let response = await myAsyncFunction();
-            console.log(response);
-
-        }catch(e){
-            console.log(e);
-        }
-        
-    }
+  const fetchProducts = async () => {
+    let response = await axios.get("https://dummyjson.com/products");
+    setProducts(response.data.products);
+  };
 
   return (
-    <div>
-      <button className="btn btn-primary">Homepage</button>
-      <br />
-      <Link to={"/about"}>About</Link>
+    <div className="container mt-5">
+      <div className="row">
+        {products.map(product => (
+          <div key = {product.id} className="col-md-3 col-12 mb-5">
+            <ProductCard product = {product} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
